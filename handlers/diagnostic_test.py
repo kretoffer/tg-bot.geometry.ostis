34a69ee68b1a88e3e28f4fa3ef4202e2f9ba8f84
kretoffer <20kretovartem000@gmail.com>
2025-09-16 19:57:44 +0300

    add registration user

--- handlers/diagnostic_test.py
+++ handlers/diagnostic_test.py
@@ -2,17 +2,18 @@ from aiogram import Router, F
 from aiogram.filters import Command
 from aiogram.types import Message, CallbackQuery
 
-from shemes.question import Question
-
 from utils.callback_filters import PrefixCallbackFilter
 from utils.get_user import get_user, get_current_test
 from utils.create_action import create_action
 
 from sc_client.constants import sc_type
-from sc_client.models import ScAddr, ScTemplate
+from sc_client.models import ScAddr, ScTemplate, ScLinkContentType
 from sc_client.client import search_by_template
 
 from sc_kpm.sc_keynodes import ScKeynodes
+from sc_kpm.utils import generate_link
+
+from keyboards.diagnostc_test import reg_classes_keyboard, get_reg_knowledge_level_keyboard
 
 diagnostic_test_router = Router()
 
@@ -27,8 +28,36 @@ async def set_answer(user: ScAddr, test: ScAddr, answer: ScAddr):
 @diagnostic_test_router.message(F.text.lower() == "пройти тест")
 @diagnostic_test_router.message(Command("test"))
 async def cmd_start_diagnostic_test(message: Message):
-    user = get_user(message.from_user.id)
-    
+    user = get_user(message.chat.id)
+    if user:
+        await start_diagnostic_test(user)
+    else:
+        await message.answer("Выберите класс:", reply_markup=reg_classes_keyboard)
+
+
+@diagnostic_test_router.callback_query(PrefixCallbackFilter("user-reg-class"))
+async def set_user_class(query: CallbackQuery):
+    user_class = query.data.split(":")[1]
+    await query.message.edit_text("Выберите свой уровень знаний:", reply_markup=get_reg_knowledge_level_keyboard(user_class))
+
+
+@diagnostic_test_router.callback_query(PrefixCallbackFilter("user-reg-kn-level"))
+async def set_user_kn_level(query: CallbackQuery):
+    [_, user_class, kn_level] = query.data.split(":")
+
+    link_user_id = generate_link(str(query.message.chat.id), ScLinkContentType.STRING, sc_type.CONST_NODE_LINK)
+    user_class_link = generate_link(user_class, ScLinkContentType.STRING, sc_type.CONST_NODE_LINK)
+    user_name_link = generate_link(query.message.chat.first_name, ScLinkContentType.STRING, sc_type.CONST_NODE_LINK)
+    user_kn_level_link = ScKeynodes.resolve(f"{kn_level}_knowledge_level", sc_type.CONST_NODE)
+
+    await create_action("action_reg_user", link_user_id, user_class_link, user_name_link, user_kn_level_link)
+
+    # MOVE TO CALLBACK
+    user = get_user(query.message.chat.id)
+    await query.message.delete()
+    start_diagnostic_test(user)
+
+async def start_diagnostic_test(user: ScAddr):    
     await create_action("action_start_test", user)
 
 
--- keyboards/diagnostc_test.py
+++ keyboards/diagnostc_test.py
@@ -9,3 +9,21 @@ def get_question_keyboard(question: Question):
     ]
 
     return InlineKeyboardMarkup(inline_keyboard=keyboard)
+
+
+reg_classes_keyboard = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text=str(i), callback_data=f"user-reg-class:{i}")]
+        for i in range(9, 12)
+    ]
+)
+
+def get_reg_knowledge_level_keyboard(user_class: int | str):
+    return InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text="Низкий", callback_data=f"user-reg-kn-level:{user_class}:bad")],
+            [InlineKeyboardButton(text="Нормальный", callback_data=f"user-reg-kn-level:{user_class}:normal")],
+            [InlineKeyboardButton(text="Высокий", callback_data=f"user-reg-kn-level:{user_class}:good")],
+            [InlineKeyboardButton(text="Олимпиадный", callback_data=f"user-reg-kn-level:{user_class}:olimpyc")]
+        ]
+    )
