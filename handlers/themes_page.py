dee900c84f20d38df84f35e383e93fca2bcd5658
kretoffer <20kretovartem000@gmail.com>
2025-09-20 17:23:36 +0300

    redesigned themes page callback

--- handlers/themes_page.py
+++ handlers/themes_page.py
@@ -12,12 +12,19 @@ themes_page_router = Router()
 
 PAGE_SIZE = 10
 
+COMPARATORS = {
+
+}
+
 
 @themes_page_router.callback_query(PrefixCallbackFilter("themes_page"))
 async def handle_page_callback(query: CallbackQuery):
     page = int(query.data.split(":")[1])
     prefix = query.data.split(":")[2]
-    themes = await get_themes_list()
+    if prefix not in COMPARATORS:
+        themes = await get_themes_list()
+    else:
+        themes = await COMPARATORS[prefix]
     indexes = [theme.value for theme in themes]
     themes = [get_name_str(theme) for theme in themes]
     await query.message.edit_reply_markup(reply_markup=get_theme_keyboard(prefix, "themes_page", themes, indexes, page=page, page_size=PAGE_SIZE, nav_postfix=prefix))
